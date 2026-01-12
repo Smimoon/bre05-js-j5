3fce523474db0a5e88e026026248434c99e2f90e
smimoon <simonlaroche@orange.fr>
2026-01-12 15:27:05 +0000

    Save work in progress before afternoon break

--- tic-tac-toe/index.js
+++ tic-tac-toe/index.js
@@ -1,3 +1,45 @@
+function isBoxEmpty(box) {
+    if (box.textContent.trim() === "") {
+        return true;
+    }
+    else {
+        return false;
+    }
+
+}
+function humanPlayer(boxes) {
+    for (let box of boxes) {
+        box.addEventListener("click", (event) => {
+            let target = event.target;
+            if (isBoxEmpty(target) === true) {
+                let cross = document.createTextNode("X");
+                box.appendChild(cross);
+                return target;
+            }
+        });
+        
+    };
+}
+function computer(boxes) {
+    
+}
+
+function restartGame(reset, boxes) {
+    reset.addEventListener("click", (event) => {
+        for(let box of boxes) {
+            box.textContent = "";
+        }
+    });
+}
+
+
 window.addEventListener('DOMContentLoaded', () => {
+    let boxes = document.querySelectorAll("#game-board div");
+    let reset = document.getElementById("reset");
+    
+    humanPlayer(boxes);
+    computer(boxes);
+    
+    restartGame(reset, boxes);
 
 });
